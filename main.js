4563a3698fe3f2d46035c0efc74720ad3d3f6316
soledy126 <132954618+soledy126@users.noreply.github.com>
2024-01-17 18:33:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+var mouseEvent = "empty";
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    widthOfLine = 2;
+
+    canvas.addEventListener("mousedown", my_mousedown);
+    
+
+    function my_mousedown(e)
+    {
+        color = document.getElementById("color").value;
+        widthOfLine = document.getElementById("widthOfLine").value;
+        radius = document.getElementById("radius").value;
+        mouseEvent = "mousedown";
+    }
+        
+    canvas.addEventListener("mousemove", my_mousemove);
+
+    function my_mousemove(e)
+    {
+        currentPositionOfMouseX = e.clientX - canvas.offsetLeft;
+        currentPositionOfMouseY = e.clientY - canvas.offsetTop;
+
+        if (mouseEvent == "mousedown") {
+            ctx.beginPath();
+            ctx.strokeStyle = color;
+            ctx.lineWidth = widthOfLine;
+    
+            console.log("Ultima posicao das coordenadas x e y = ");
+            console.log("x = " + lastPositionOfX + "y = " + lastPositionOfY);
+            ctx.moveTo(lastPositionOfX, lastPositionOfY);
+    
+            console.log("Posicao atual das coordenadas x e y = ");
+            console.log("x  = " + currentPositionOfMouseX + "y = " + currentPositionOfMouseY);
+            ctx.lineTo(currentPositionOfMouseX, currentPositionOfMouseY);
+            ctx.stroke();
+            }
+    
+            lastPositionOfX = currentPositionOfMouseX; 
+            lastPositionOfY = currentPositionOfMouseY;
+
+    }
+    canvas.addEventListener("mouseup", my_mouseup);
+
+    function my_mouseup(e)
+    {
+
+        mouseEvent = "mouseup";
+    }
+    canvas.addEventListener("mouseleave", my_mouseleave);
+
+    function my_mouseleave(e)
+    {
+
+        mouseEvent = "mouseleave";
+    }
+
+    function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
